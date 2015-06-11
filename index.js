3666efd74868d4b1623bf9c0f4b70392dcc8cc99
Diego Betto <d.betto@mvlabs.it>
2015-06-11 16:29:26 +0200

    /DELETE reservation

--- index.js
+++ index.js
@@ -34,17 +34,20 @@ var server;
 	        if (err) {
 	            console.error('error fetching users from pool', err);
 	            return fn(null, null);
-	        }console.log(user,pass);
-	        client.query('SELECT name,password,surname,gender,country,province,town,address,zip,phone,mobile,pin,discount_rate FROM customers WHERE email=$1 AND password=$2 LIMIT 1', [user, pass], function(err, result) {
-	            // release the client back to the pool
-	            done();
+	        }
+	        client.query('SELECT id,name,password,surname,gender,country,province,town,address,zip_code,phone,mobile,pin,discount_rate FROM customers WHERE email=$1 AND password=$2 LIMIT 1', 
+	        	[user, pass], 
+	        	function(err, result) {
+	            	// release the client back to the pool
+		            done();
 
-	            if (err) {
-	                console.error('error running query', err);
-	                return fn(null, null);
-	            }
-	            return fn(null, result.rows[0]);
-	        });
+		            if (err) {
+		                console.error('error running query', err);
+		                return fn(null, null);
+		            }
+		            return fn(null, result.rows[0]);
+	        	}
+	        );
 	    });
 	}
 
@@ -54,9 +57,10 @@ var server;
 	 */
 	passport.use(new BasicStrategy({},
 	    function(username, password, done) {
+	    	console.log(username,password);
 	        process.nextTick(function() {
-	        	console.log(asd);
 	            validateUser(username, password, function(err, user) {
+
 	                if (err) {
 	                    return done(err);
 	                }
@@ -149,17 +153,26 @@ var server;
 	);
 
 	server.get(
-		'/v2/cars:plate', 
+		'/v2/cars/:plate', 
 		passport.authenticate('basic', {session: false}), 
 		getCars
 	);
 
 
-	server.get('/v2/reservations', getReservations);
+	server.get(
+		'/v2/reservations', 
+		passport.authenticate('basic', {session: false}), 
+		getReservations
+	);
+
 	server.get('/v2/trips', getTrips);
 	server.get('/v2/trips/:id', getTrips);
 
-	server.post('/v2/reservations', postReservations);
+	server.post(
+		'/v2/reservations', 
+		passport.authenticate('basic', {session: false}), 
+		postReservations
+	);
 
 	server.del('/v2/reservations/:id', delReservations);
 
@@ -176,7 +189,9 @@ var server;
 	 * @param  function next handler
 	 */
 	function getUser(req, res, next) {
-	    sendOutJSON(200,'',req.user);
+		delete req.user.id;
+		delete req.user.password;
+	    sendOutJSON(res,200,'',req.user);
 	    return next();
 	}
 
@@ -187,12 +202,18 @@ var server;
 	            return false;
 	        }
 
-	        var query = '',params;
-	        if(req.params.plate === undefined){
-	        	query = "SELECT plate,model,maker,lat,lon,internal_cleanliness,external_cleanliness,fuel_percentage FROM cars WHERE ST_Distance_Sphere(ST_SetSRID(ST_MakePoint(longitude, latitude), 4326),ST_SetSRID(ST_MakePoint($2, $1), 4326)) < $3";
-	        	params = [req.headers.lat, req.headers.lon, req.headers.radius];
+	        var query = '',params,queryStatus;
+	        if(typeof  req.params.plate === 'undefined'){
+	        	if(typeof req.headers.status !== 'undefined'){
+	        		queryStatus = ' AND status = $4';
+	        		params = [req.headers.lat, req.headers.lon, req.headers.radius, req.headers.status];
+	        	}else{
+	        		queryStatus = '';
+	        		params = [req.headers.lat, req.headers.lon, req.headers.radius];
+	        	}
+	        	query = "SELECT plate,model,manufactures as maker,latitude as lat,longitude as lon,int_cleanliness as internal_cleanliness,ext_cleanliness as external_cleanliness,battery as fuel_percentage FROM cars WHERE ST_Distance_Sphere(ST_SetSRID(ST_MakePoint(longitude, latitude), 4326),ST_SetSRID(ST_MakePoint($2, $1), 4326)) < $3 " + queryStatus;
 	        }else{
-	        	query = "SELECT plate,model,maker,lat,lon,internal_cleanliness,external_cleanliness,fuel_percentage FROM cars WHERE plate = $1";
+	        	query = "SELECT plate,model,manufactures as maker,latitude as lat,longitude as lon,int_cleanliness as internal_cleanliness,ext_cleanliness as external_cleanliness,battery as fuel_percentage FROM cars WHERE plate = $1";
 	        	params = [req.params.plate];
 	        }
 
@@ -206,7 +227,7 @@ var server;
 		                console.error('error running query', err);
 		                return false;
 		            }
-		            sendOutJSON(200,'',result.rows);
+		            sendOutJSON(res,200,'',result.rows);
 		           
 	        	}
 	        );
@@ -215,7 +236,27 @@ var server;
 	}
 
 	function getReservations(req, res, next) {
-	    res.send(req.params);
+		pg.connect(conString, function(err, client, done) {
+	        if (err) {
+	            console.error('error fetching reservations from pool', err);
+	            return false;
+	        }
+
+	        client.query(
+	        	"SELECT id,extract(epoch from ts::timestamp with time zone)::integer as reservation_timestamp,extract(epoch from beginning_ts::timestamp with time zone)::integer as timestamp_start,active as is_active FROM reservations WHERE customer_id = $1", 
+	        	[req.user.id], 
+	        	function(err, result) {
+		            done();
+
+		            if (err) {
+		                console.error('error running query', err);
+		                return false;
+		            }
+		            sendOutJSON(res,200,'',result.rows);
+		           
+	        	}
+	        );
+	    });
 	    return next();
 	}
 
@@ -234,11 +275,26 @@ var server;
 	 * @param  function next handler
 	 */
 	function postReservations(req, res, next) {
-	    res.send({
-	        'Active': req.headers.active,
-	        'Quantity': req.headers.quantity,
-	        'From': req.headers.from,
-	        'To': req.headers.to,
+		pg.connect(conString, function(err, client, done) {
+	        if (err) {
+	            console.error('error adding reservation from pool', err);
+	            return false;
+	        }
+	        console.log(req.user);
+	        client.query(
+	        	"INSERT INTO reservations (ts,car_plate,customer_id,beginning_ts,active,length,to_send,sent_ts) VALUES (NOW(),$1,$2,NOW(),true,1,true,NOW())", 
+	        	[req.params.plate,req.user.id], 
+	        	function(err, result) {
+		            done();
+
+		            if (err) {
+		                console.error('error running query', err);
+		                return false;
+		            }
+		            sendOutJSON(res,200,'','');
+		           
+	        	}
+	        );
 	    });
 	    return next();
 	}
@@ -278,7 +334,7 @@ var server;
 
 /* EXTRA FUNCTIONS */
 
-	function sendOutJSON(status,reason,data){
+	function sendOutJSON(res,status,reason,data){
 	    res.send(status, {
 	        'status': status,
 	        'reason': reason,
